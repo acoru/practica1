a8060efaf398ef9ba828caf5e536745386005a72
acoru <acr00073@red.ujaen.es>
2015-10-06 19:46:11 +0200

    Better send and recieve control error and minor changes
    Sending and recieving error:
    - Now, if the client close the client application, the server won't
    close, it'll be waiting for a new connection from a client.
    Minor changes:
    - Remove the number control while inserting the numbers that will be
    send to the server, now it's only controlled on the server side
    - Change the way the server check the SUM format for the SUM
    functionality, now it's simpler and easier to understand the code and
    how it works

--- server/servidor.c
+++ server/servidor.c
@@ -36,10 +36,6 @@ main()
 	int recibidos=0,enviados=0;
 	int estado=0;
 	//NEW VARIABLES FOR SUM FROM HERE
-	//cSum fSpace and sSpace will be use for comparing the correct format of the SUM functionality
-	char cSum[4] = "", fSpace[2] = "", sSpace[2] = "", cCRLF[3] = "";
-	//cNum1 and cNum2 same as the other chars written before, but this will be use for the numbers
-	char cNum1[5] = "", cNum2[5] = "";
 	//here is where the numbers will be saved before the SUM
 	int num1 = 0, num2 = 0;
 	int suma = 0;
@@ -109,8 +105,8 @@ main()
 			if(enviados < 0)
 			{
 				//-7 value chosen for send error code
-				return(-7);
-				printf("SERVER> ERROR SENDING DATA %s%s", error, CRLF);
+				//return(-7);
+				printf("SERVER> ERROR SENDING DATA %d%s", error, CRLF);
 				//provisional solution for error sending welcome message to the client, just close the socket
 				fin_conexion = 1;
 			}
@@ -136,8 +132,8 @@ main()
 				{
 					DWORD error = GetLastError();
 					//-8 value chosen for recv(reception) error code
-					return(-8);
-					printf("SERVER> ERROR ON RECEPTION%s%s", error, CRLF);
+					//return(-8);
+					printf("SERVER> ERROR ON RECEPTION%d%s", error, CRLF);
 					//provisional solution for reciving error from client, just close the socket
 					fin_conexion = 1;
 				}
@@ -146,155 +142,143 @@ main()
 					fin_conexion = 1;
 				}
 			}
-			
-			buffer_in[recibidos] = 0x00;
-			printf ("SERVIDOR [bytes recibidos]> %d\r\nSERVIDOR [datos recibidos]>%s", recibidos, buffer_in);
-			
-			switch (estado)
+			else
 			{
-				case S_USER:    /*****************************************/
-					strncpy_s ( cmd, sizeof(cmd),buffer_in, 4);
-					cmd[4]=0x00; // en C los arrays finalizan con el byte 0000 0000
+				buffer_in[recibidos] = 0x00;
+				printf ("SERVIDOR [bytes recibidos]> %d\r\nSERVIDOR [datos recibidos]>%s", recibidos, buffer_in);
+			
+				switch (estado)
+				{
+					case S_USER:    /*****************************************/
+						strncpy_s ( cmd, sizeof(cmd),buffer_in, 4);
+						cmd[4]=0x00; // en C los arrays finalizan con el byte 0000 0000
 
-					if ( strcmp(cmd,SC)==0 ) // si recibido es solicitud de conexion de aplicacion
-					{
-						sscanf_s (buffer_in,"USER %s\r\n",usr,sizeof(usr));
-						
-						// envia OK acepta todos los usuarios hasta que tenga la clave
-						sprintf_s (buffer_out, sizeof(buffer_out), "%s%s", OK,CRLF);
-						
-						estado = S_PASS;
-						printf ("SERVIDOR> Esperando clave\r\n");
-					} else
-					if ( strcmp(cmd,SD)==0 )
-					{
-						sprintf_s (buffer_out, sizeof(buffer_out), "%s Fin de la conexión%s", OK,CRLF);
-						fin_conexion=1;
-					}
-					else
-					{
-						sprintf_s (buffer_out, sizeof(buffer_out), "%s Comando incorrecto%s",ER,CRLF);
-					}
-				break;
-
-				case S_PASS: /******************************************************/
-
-					
-					strncpy_s ( cmd, sizeof(cmd), buffer_in, 4);
-					cmd[4]=0x00; // en C los arrays finalizan con el byte 0000 0000
-
-					if ( strcmp(cmd,PW)==0 ) // si comando recibido es password
-					{
-						sscanf_s (buffer_in,"PASS %s\r\n",pas,sizeof(usr));
-
-						if ( (strcmp(usr,USER)==0) && (strcmp(pas,PASSWORD)==0) ) // si password recibido es correcto
+						if ( strcmp(cmd,SC)==0 ) // si recibido es solicitud de conexion de aplicacion
 						{
-							// envia aceptacion de la conexion de aplicacion, nombre de usuario y
-							// la direccion IP desde donde se ha conectado
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s %s IP(%s)%s", OK, usr, inet_ntoa(remote_addr.sin_addr),CRLF);
-							estado = S_DATA;
-							printf ("SERVIDOR> Esperando comando\r\n");
+							sscanf_s (buffer_in,"USER %s\r\n",usr,sizeof(usr));
+						
+							// envia OK acepta todos los usuarios hasta que tenga la clave
+							sprintf_s (buffer_out, sizeof(buffer_out), "%s%s", OK,CRLF);
+						
+							estado = S_PASS;
+							printf ("SERVIDOR> Esperando clave\r\n");
+						} else
+						if ( strcmp(cmd,SD)==0 )
+						{
+							sprintf_s (buffer_out, sizeof(buffer_out), "%s Fin de la conexión%s", OK,CRLF);
+							fin_conexion=1;
 						}
 						else
 						{
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s Autenticación errónea%s",ER,CRLF);
+							sprintf_s (buffer_out, sizeof(buffer_out), "%s Comando incorrecto%s",ER,CRLF);
 						}
-					} else
-					if ( strcmp(cmd,SD)==0 )
-					{
-						sprintf_s (buffer_out, sizeof(buffer_out), "%s Fin de la conexión%s", OK,CRLF);
-						fin_conexion=1;
-					}
-					else
-					{
-						sprintf_s (buffer_out, sizeof(buffer_out), "%s Comando incorrecto%s",ER,CRLF);
-					}
-				break;
+					break;
 
-				case S_DATA: /***********************************************************/
-					
-					buffer_in[recibidos] = 0x00;
-					
-					strncpy_s(cmd,sizeof(cmd), buffer_in, 4);
+					case S_PASS: /******************************************************/
 
-					printf ("SERVIDOR [Comando]>%s\r\n",cmd);
 					
-					if ( strcmp(cmd,SD)==0 )
-					{
-						sprintf_s (buffer_out, sizeof(buffer_out), "%s Fin de la conexión%s", OK,CRLF);
-						fin_conexion=1;
-					}
-					else if (strcmp(cmd,SD2)==0)
-					{
-						sprintf_s (buffer_out, sizeof(buffer_out), "%s Finalizando servidor%s", OK,CRLF);
-						fin_conexion=1;
-						fin=1;
-					}
-					//NEW CODE ADDED from here for SUM
-					else if(strcmp(cmd, "SUM ") == 0)
-					{
-						strncpy(cSum, buffer_in, 3);
-						cSum[3] = 0x00;
-						strncpy(fSpace, buffer_in + 3, 1);
-						fSpace[1] = 0x00;
-						strncpy(sSpace, buffer_in + 8, 1);
-						sSpace[1] = 0x00;
-						strncpy(cNum1, buffer_in + 4, 4);
-						cNum1[4] = 0x00;
-						strncpy(cNum2, buffer_in + 9, 4);
-						cNum2[4] = 0x00;
-						strncpy(cCRLF, buffer_in + 13, 2);
-						cCRLF[2] = 0x00;
-						sscanf_s(cNum1, "%d", &num1);
-						sscanf_s(cNum2, "%d", &num2);
-						if(strcmp(cSum, SUM) == 0 && strcmp(fSpace, " ") == 0 && strcmp(sSpace, " ") == 0 && strcmp(cCRLF, CRLF) == 0 && num1 > 0 && num1 < 9999 && num2 > 0 && num2 < 9999)
+						strncpy_s ( cmd, sizeof(cmd), buffer_in, 4);
+						cmd[4]=0x00; // en C los arrays finalizan con el byte 0000 0000
+
+						if ( strcmp(cmd,PW)==0 ) // si comando recibido es password
 						{
-							suma = num1 + num2;
-							printf("%d", suma);
-							sprintf_s(buffer_out, sizeof(buffer_out), "%s %d%s", OK, suma, CRLF);
+							sscanf_s (buffer_in,"PASS %s\r\n",pas,sizeof(usr));
+
+							if ( (strcmp(usr,USER)==0) && (strcmp(pas,PASSWORD)==0) ) // si password recibido es correcto
+							{
+								// envia aceptacion de la conexion de aplicacion, nombre de usuario y
+								// la direccion IP desde donde se ha conectado
+								sprintf_s (buffer_out, sizeof(buffer_out), "%s %s IP(%s)%s", OK, usr, inet_ntoa(remote_addr.sin_addr),CRLF);
+								estado = S_DATA;
+								printf ("SERVIDOR> Esperando comando\r\n");
+							}
+							else
+							{
+								sprintf_s (buffer_out, sizeof(buffer_out), "%s Autenticación errónea%s",ER,CRLF);
+							}
+						} else
+						if ( strcmp(cmd,SD)==0 )
+						{
+							sprintf_s (buffer_out, sizeof(buffer_out), "%s Fin de la conexión%s", OK,CRLF);
+							fin_conexion=1;
 						}
 						else
 						{
-							sprintf_s(buffer_out, sizeof(buffer_out), "%s%s", ER, CRLF);
+							sprintf_s (buffer_out, sizeof(buffer_out), "%s Comando incorrecto%s",ER,CRLF);
 						}
-					}
-					//END OF NEW CODE
-					//NEW CODE ADDED FOR ECHO FUNCTIONALITY
-					else if(strcmp(cmd, ECHO) == 0)
+					break;
+
+					case S_DATA: /***********************************************************/
+					
+						buffer_in[recibidos] = 0x00;
+					
+						strncpy_s(cmd,sizeof(cmd), buffer_in, 4);
+
+						printf ("SERVIDOR [Comando]>%s\r\n",cmd);
+					
+						if ( strcmp(cmd,SD)==0 )
+						{
+							sprintf_s (buffer_out, sizeof(buffer_out), "%s Fin de la conexión%s", OK,CRLF);
+							fin_conexion=1;
+						}
+						else if (strcmp(cmd,SD2)==0)
+						{
+							sprintf_s (buffer_out, sizeof(buffer_out), "%s Finalizando servidor%s", OK,CRLF);
+							fin_conexion=1;
+							fin=1;
+						}
+						//NEW CODE ADDED from here for SUM
+						else if(strcmp(cmd, "SUM ") == 0)
+						{
+							sscanf_s(buffer_in, "SUM %d %d\r\n", &num1, &num2);
+							if(num1 > 0 && num1 < 9999 && num2 > 0 && num2 < 9999)
+							{
+								suma = num1 + num2;
+								printf("%d", suma);
+								sprintf_s(buffer_out, sizeof(buffer_out), "%s %d%s", OK, suma, CRLF);
+							}
+							else
+							{
+								sprintf_s(buffer_out, sizeof(buffer_out), "%s%s", ER, CRLF);
+							}
+						}
+						//END OF NEW CODE
+						//NEW CODE ADDED FOR ECHO FUNCTIONALITY
+						else if(strcmp(cmd, ECHO) == 0)
+						{
+							sprintf_s(buffer_out, sizeof(buffer_out), "%s %s %s", OK, buffer_in + 5, CRLF);
+						}
+						//END OF NEW CODE
+						else
+						{
+							sprintf_s (buffer_out, sizeof(buffer_out), "%s Comando incorrecto%s",ER,CRLF);
+						}
+						break;
+
+					default:
+						break;
+					
+				} // switch
+
+				enviados=send(nuevosockfd,buffer_out,(int)strlen(buffer_out),0);
+				//TODO 
+				if(enviados <= 0)
+				{
+					if(enviados < 0)
 					{
-						sprintf_s(buffer_out, sizeof(buffer_out), "%s %s %s", OK, buffer_in + 5, CRLF);
+						DWORD error = GetLastError();
+						//if an error occurs sending data from the state machine, it will return as error code "-9"
+						//return(-9);
+						printf("SERVER> ERROR SENDING DATA: %d%s", error, CRLF);
+						//provisional solution for sending error, just close the socket created for the current client conection
+						fin_conexion = 1;
 					}
-					//END OF NEW CODE
 					else
 					{
-						sprintf_s (buffer_out, sizeof(buffer_out), "%s Comando incorrecto%s",ER,CRLF);
+						fin_conexion = 1;
 					}
-					break;
-
-				default:
-					break;
-					
-			} // switch
-
-			enviados=send(nuevosockfd,buffer_out,(int)strlen(buffer_out),0);
-			//TODO 
-			if(enviados <= 0)
-			{
-				if(enviados < 0)
-				{
-					DWORD error = GetLastError();
-					//if an error occurs sending data from the state machine, it will return as error code "-9"
-					return(-9);
-					printf("SERVER> ERROR SENDING DATA: %s%s", error, CRLF);
-					//provisional solution for sending error, just close the socket created for the current client conection
-					fin_conexion = 1;
-				}
-				else
-				{
-					fin_conexion = 1;
 				}
 			}
-
 
 		} while (!fin_conexion);
 		printf ("SERVIDOR> CERRANDO CONEXION DE TRANSPORTE\r\n");
